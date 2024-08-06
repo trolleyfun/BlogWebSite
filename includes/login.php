bbda3061700d62c03acf4834f0da041e63a93409
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-08-06 12:54:20 +0500

    Add login validation

---
+++ includes/login.php
@@ -0,0 +1,33 @@
+<?php include "db.php"; ?>
+<?php session_start(); ?>
+
+<?php 
+if (isset($_POST['login_btn'])) {
+    $received_login = $_POST['user_login'];
+    $received_password = $_POST['user_password'];
+
+    $received_login = mysqli_real_escape_string($connection, $received_login);
+    $received_password = mysqli_real_escape_string($connection, $received_password);
+
+    $query = "SELECT * FROM users WHERE user_login = '{$received_login}';";
+    $userLogin = mysqli_query($connection, $query);
+    if ($row = mysqli_fetch_assoc($userLogin)) {
+        $user_id = $row['user_id'];
+        $user_login = $row['user_login'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image_name = $row['user_image'];
+        $user_privilege = $row['user_privilege'];
+        
+        if ($user_login == $received_login && $user_password == $received_password) {
+            header("Location: ../admin/");
+        } else {
+            header("Location: ../index.php");
+        }
+    } else {
+        header("Location: ../index.php");
+    }
+}
+?>
