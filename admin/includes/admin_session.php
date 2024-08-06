0a354ab3d403e3266e52db78f6a5e7ced807e5c2
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-08-06 13:34:29 +0500

    Add validation on user privilege

--- admin/includes/admin_session.php
+++ admin/includes/admin_session.php
@@ -1,11 +1,28 @@
 <?php
 session_start();
 
-$session_user_id = $_SESSION['user_id'];
-$session_user_login = $_SESSION['login'];
-$session_user_firstname = $_SESSION['firstname'];
-$session_user_lastname = $_SESSION['lastname'];
-$session_user_email = $_SESSION['email'];
-$session_user_image_name = $_SESSION['image'];
-$session_user_privilege = $_SESSION['privilege'];
+if (isset($_SESSION['login'])) {
+    $session_user_login = $_SESSION['login'];
+} else {
+    $session_user_login = "Пользователь";
+}
+if (isset($_SESSION['firstname'])) {
+    $session_user_firstname = $_SESSION['firstname'];
+} else {
+    $session_user_firstname = "Имя";
+}
+if (isset($_SESSION['lastname'])) {
+    $session_user_lastname = $_SESSION['lastname'];
+} else {
+    $session_user_lastname = "Фамилия";
+}
+if (isset($_SESSION['privilege'])) {
+    $session_user_privilege = $_SESSION['privilege'];
+} else {
+    $session_user_privilege = "пользователь";
+}
+
+if ($session_user_privilege != "администратор") {
+    header("Location: ../index.php");
+}
 ?>
