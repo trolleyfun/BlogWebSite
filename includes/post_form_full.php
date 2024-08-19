e93f632afd194384b42f7ba3ffcbfe7dd6d19068
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-08-19 16:47:28 +0500

    Remove author link from the Post Form

--- includes/post_form_full.php
+++ includes/post_form_full.php
@@ -3,12 +3,12 @@
     <a href="post.php?post_id=<?=$post_id;?>"><?=$post_title;?></a>
 </h2>
 <p class="lead">
-    автор: <a href="index.php"><?=$post_author;?></a>
+    автор: <?=$post_author;?>
 </p>
 <p><span class="glyphicon glyphicon-time"></span> Опубликовано <?=$post_date;?></p>
 <hr>
 <a href="post.php?post_id=<?=$post_id;?>">
-    <img class="img-responsive" src="img/<?=$post_image;?>" alt="<?=$post_title;?>" style="max-width: 80%;">
+    <img class="img-responsive" src="img/<?=$post_image;?>" alt="<?=$post_title;?>" style="width: 80%;">
 </a>
 <hr>
 <p><?=$post_content;?></p>
